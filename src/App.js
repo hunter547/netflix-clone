5664723bf38a6b5aaaceca42e26446b2885070c1
Hunter Evanoff <hunter547@gmail.com>
2021-03-13 17:45:10 -0700

    Setup the general structure of the jumbotron on the landing page of the Netflix login.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from 'react';
+import Jumbotron from './components/jumbotron';
+import jumbotronData from './fixtures/jumbo.json';
 function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron.Container>
+      {jumbotronData.map(item => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>       
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
